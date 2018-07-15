0f1acb736fb8d10e5cd9a1fcd1f959463dbb7eed
venaticus629 <venaticus629@gmail.com>
2018-07-15 19:31:15 +0900

    This IS Rich Turtle Message Input!!

---
+++ RichTurtle/RichTurtle/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RichTurtle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This Is Rich Turtle!!!");
+        }
+    }
+}
